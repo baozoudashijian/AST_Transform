932207cb1dca9f852fc30d77848444425578efff
Roger Zhang <1787009521@qq.com>
2022-07-09 21:25:13 +0800

    feat: 完成let转成var

--- let_to_var.ts
+++ let_to_var.ts
@@ -1,9 +1,19 @@
+// 运行代码：node -r ts-node/register --inspect-brk let_to_var.ts
 import { parse } from "@babel/parser"
 import traverse from "@babel/traverse"
 import generate from "@babel/generator"
 
 const code = `let a = 'let'; let b = 2`
-console.log(1)
 const ast = parse(code, { sourceType: 'module' })
+traverse(ast, {
+    enter: item => {
+        if(item.node.type === 'VariableDeclaration') {
+            item.node.kind = 'var'
+        }
+    }
+})
+const result = generate(ast, {}, code)
 
-console.log(JSON.parse(JSON.stringify(ast)))
+console.log(ast)
+console.log(result)
+console.log(result.code)
